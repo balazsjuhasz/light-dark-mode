53418badc744db503f0c40db5bb90c28f459e62d
Balazs Juhasz <juhaszbal@gmail.com>
2020-12-23 13:52:09 +0100

    make code cleaner

--- script.js
+++ script.js
@@ -6,29 +6,26 @@ const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
 
-/// Dark or Light Images
+// Dark or Light Images
 const imageMode = (color) => {
   image1.src = `img/undraw_proud_coder_${color}.svg`;
   image2.src = `img/undraw_feeling_proud_${color}.svg`;
   image3.src = `img/undraw_conceptual_idea_${color}.svg`;
 };
 
-// Dark Mode Styles
-const darkMode = () => {
-  nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-  textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-  toggleIcon.children[0].textContent = 'Dark Mode';
-  toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
-  imageMode('dark');
-};
-
-// Light Mode Styles
-const lightMode = () => {
-  nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-  textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-  toggleIcon.children[0].textContent = 'Light Mode';
-  toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-  imageMode('light');
+// Toggle Dark And Light MOde
+const toggleDarkLightMode = (isDark) => {
+  nav.style.backgroundColor = isDark
+    ? 'rgb(0 0 0 / 50%)'
+    : 'rgb(255 255 255 / 50%)';
+  textBox.style.backgroundColor = isDark
+    ? 'rgb(255 255 255 / 50%)'
+    : 'rgb(0 0 0 / 50%)';
+  toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
+  isDark
+    ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon')
+    : toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+  isDark ? imageMode('dark') : imageMode('light');
 };
 
 // Switch Theme Dynamically
@@ -36,11 +33,11 @@ const switchTheme = (event) => {
   if (event.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
     localStorage.setItem('theme', 'dark');
-    darkMode();
+    toggleDarkLightMode(true);
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
     localStorage.setItem('theme', 'light');
-    lightMode();
+    toggleDarkLightMode(false);
   }
 };
 
@@ -54,6 +51,6 @@ if (currentTheme) {
 
   if (currentTheme === 'dark') {
     toggleSwitch.checked = true;
-    darkMode();
+    toggleDarkLightMode(true);
   }
 }
